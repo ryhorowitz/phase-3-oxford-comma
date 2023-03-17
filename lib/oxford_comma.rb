075d964beb7bfa7a7cb81f5b5d42e65b95bf74db
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-03-17 14:02:22 -0400

    commit tree 30593b2737ae3dd2a099ce4258cfe50c3a693515

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
+require 'pry'
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+# => "fiddleheads, okra, and kohlrabi"
 def oxford_comma(array)
-
+  result = ""
+  if array.size == 1 then result << array[0]
+  elsif array.size == 2 then result << "#{array[0]} and #{array[1]}" 
+  else 
+    array.each_with_index do |w, i| 
+      last_index = array[i] == array[-1]
+      last_index ? 
+        result << "and #{w}" 
+        : result << w + ', '
+      # binding.pry
+    end
+  end
+  result
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
